98fa575dfee9bc5eb099337525a5a74623fb54db
SuperGAN-Public <supergan-email@protonmail.com>
2021-06-06 19:06:39 -0700

    Fix labels for adlnormal so everything is now 0-based

--- do_experiments.rb
+++ do_experiments.rb
@@ -30,7 +30,7 @@ ENDTOML
 
 # Mapping of dataset names to the available labels
 DATASET_LABELS = {
-  'adlnormal' => [1, 2, 3, 4, 5],
+  'adlnormal' => [0, 1, 2, 3, 4],
   'gyroscope' => [0, 1, 2, 3, 4, 5, 6, 7, 8],
   'accelerometer' => [0, 1, 2, 3, 4, 5, 6, 7, 8]
 }
